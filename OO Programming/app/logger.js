47b6db8ba8e794fabf8c151649b006afdb9b5135
Maks Kremez <mr.kremez@gmail.com>
2015-06-18 12:36:56 +0300

    basic structure and methods of Logger class. initializer via value or name

--- OO Programming/app/logger.js
+++ OO Programming/app/logger.js
@@ -1,7 +1,70 @@
 define(function() {
-  return {
-    sayHi: function() {
-      console.log('Hello');
+  'use strict'; 
+  var logLevels = [
+    { name: 'debug', value: 0 },
+    { name: 'info', value: 1 }
+  ];
+
+  function Logger(initLogLevel) {
+    setLogLevel.call(this, initLogLevel);
+  }
+
+  Logger.prototype = {
+    constructor: Logger,
+    debug: function(text) {
+      if(this.logLevel.value === 0) {
+        displayText(text, 'debug');
+      }
+    },
+    info: function(text) {
+      if(this.logLevel.value <= 1) {
+        displayText(text, 'info');
+      }
+    },
+    displayLoggerInfo: function() {
+      console.log('Log level: ' + this.logLevel.name + ';');
     }
   };
+
+  function displayText(text, logName) {
+    console.log(logName.toUpperCase() + ' - ' + text);
+  }
+
+  function isNumber(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+  }
+
+  function setLogLevel(lvl) {
+    if(isNumber(lvl)) {
+      setLogLevelByValue.call(this, lvl);
+    } else if(typeof(lvl) === 'string') {
+      setLogLevelByName.call(this, lvl);
+    } else {
+      throw 'Wrong type of initial value for Logger';
+    }
+  }
+
+  function setLogLevelByValue(lvl) {
+    for(var i = 0, len = logLevels.length; i < len; i++) {
+      if(logLevels[i].value === lvl) {
+        this.logLevel = logLevels[i];
+        break;
+      }
+    }
+    if(!this.logLevel)
+      throw 'There are no log level with value ' + lvl;
+  }
+
+  function setLogLevelByName(lvl) {
+    for(var i = 0, len = logLevels.length; i < len; i++) {
+      if(logLevels[i].name === lvl) {
+        this.logLevel = logLevels[i];
+        break;
+      }
+    }
+    if(!this.logLevel)
+      throw 'There are no log level with name ' + lvl;
+  }
+
+  return Logger;
 });
--- OO Programming/app/main.js
+++ OO Programming/app/main.js
@@ -1,5 +1,8 @@
 define(function(require) {
-  var logger = require('./logger');
-
-  logger.sayHi();
+  'use strict';
+  var Logger = require('./logger');
+  var newLogger = new Logger('debug');
+  newLogger.displayLoggerInfo();
+  newLogger.debug('Some debug information!');
+  newLogger.info('Some info information!');
 });
